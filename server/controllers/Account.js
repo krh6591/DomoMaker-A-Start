4bbeed2e75a4a075eaf6291cd263d0e1465ac945
Mira <krh6591@rit.edu>
2020-04-10 22:24:42 -0300

    Did D

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,10 +6,6 @@ const loginPage = (req, res) => {
   res.render('login', { csrfToken: req.csrfToken() });
 };
 
-const signupPage = (req, res) => {
-  res.render('signup', { csrfToken: req.csrfToken() });
-};
-
 const logout = (req, res) => {
   req.session.destroy();
   res.redirect('/');
@@ -82,10 +78,21 @@ const signup = (request, response) => {
   });
 };
 
+const getToken = (request, response) => {
+  const req = request;
+  const res = response;
+  
+  const csrfJSON = {
+    csrfToken: req.csrfToken()
+  };
+  
+  res.json(csrfJSON);
+}
+
 module.exports = {
   loginPage,
   login,
   logout,
-  signupPage,
   signup,
+  getToken
 };
